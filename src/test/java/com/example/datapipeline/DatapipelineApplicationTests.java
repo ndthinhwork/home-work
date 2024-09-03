02ff2eeae1307843c838a1581bfb4480eeb4f113
ndthih <ndthinhwork@gmail.com>
2024-09-03 23:48:54 +0700

    build: init src

---
+++ src/test/java/com/example/datapipeline/DatapipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datapipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatapipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
